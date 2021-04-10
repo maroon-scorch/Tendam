2e5131d8c979af80fce13d689e5ae0a02833bb07
maroon-scorch <matthew_ji@brown.edu>
2021-04-10 00:16:52 -0400

    Inserted Profile Picture to Database

--- gui/src/components/Profile/Profile.js
+++ gui/src/components/Profile/Profile.js
@@ -3,7 +3,12 @@ import { Button, Typography, TextField } from '@material-ui/core';
 import { useAuth } from "../../context/AuthContext";
 import { useDatabase } from "../../context/DatabaseContext";
 
+import { Link, useHistory } from 'react-router-dom';
+
 import './Profile.css';
+
+import Aos from 'aos';
+import "aos/dist/aos.css";
 
 function Profile() {
     const { currentUser } = useAuth();
@@ -15,6 +20,8 @@ function Profile() {
     const [isEditing, setIsEditing] = useState(false);
 
     useEffect(() => {
+        Aos.init({});
+
         if (currentUser !== null) {
             getEntry(currentUser.uid).then((doc) => {
                 if (doc.exists) {
@@ -58,8 +65,11 @@ function Profile() {
     }
 
     return (
-        <div className="profile-container">
+        <div className="profile-container" data-aos="fade-up" data-aos-duration="2000">
             <div className="profile">
+            <Link>
+                <img className="profile-picture" src="blank-profile.png" alt="Avatar" />
+            </Link>
             Profile Page:
             <Typography variant="h5">Bio: <TextField label="Bio"
             name="bio" value={profileInfo.bio} disabled={!isEditing} onChange={handleChange}/></Typography>
@@ -70,9 +80,13 @@ function Profile() {
             <Typography variant="h5">Age: <TextField label="Age"
             name="age" value={profileInfo.age} disabled={!isEditing} onChange={handleChange}/></Typography>
             <br />
-            <Typography variant="h5">Matches: {profileInfo.matches.length}</Typography>
             <Button variant="contained" color="primary" disabled={isEditing} onClick={handleEdit}>EDIT</Button> 
-            <Button variant="contained" color="secondary" disabled={!isEditing} onClick={handleSave}>SAVE</Button> 
+            <Button variant="contained" color="secondary" disabled={!isEditing} onClick={handleSave}>SAVE</Button>
+            <br />
+            <br />
+            <Button variant="contained" color="secondary">
+                <Typography variant="h5">Matches: {profileInfo.matches.length}</Typography>
+            </Button>
             </div>
         </div>
     )

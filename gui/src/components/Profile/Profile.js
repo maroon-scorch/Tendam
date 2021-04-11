662e07180eea506a82dfb82f120979f53d54380c
maroon-scorch <matthew_ji@brown.edu>
2021-04-11 05:15:06 -0400

    Profile Picture Upload Complete, need to fix on displaying matches

--- gui/src/components/Profile/Profile.js
+++ gui/src/components/Profile/Profile.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Button, Typography, TextField, Tooltip } from '@material-ui/core';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import { useAuth } from "../../context/AuthContext";
 import { useDatabase } from "../../context/DatabaseContext";
+import { useModal } from "../../context/ModalContext";
 
 import { Link, useHistory } from 'react-router-dom';
 
@@ -13,9 +15,10 @@ import "aos/dist/aos.css";
 function Profile() {
     const { currentUser } = useAuth();
     const { getEntry, setEntry, uploadStorage, getFile } = useDatabase();
+    const { close, open, setContent } = useModal();
     
     const [profileInfo, setProfileInfo] = useState({
-        bio: "", name: "", age: "", matches: []});
+        bio: "", name: "", age: "", id: "", matches: []});
 
     
     const selectFile = useRef(null);
@@ -42,7 +45,7 @@ function Profile() {
             getFile(currentUser.uid, '/profilePicture').then((imageFile) => {
                 setProfilePic(imageFile);
             }).catch((error) => {
-                console.log("Error getting document:", error);
+                // console.log("Error getting document:", error);
             });
 
         }
@@ -95,6 +98,35 @@ function Profile() {
         selectFile.current.click();
     }
 
+    async function displayMatch() {
+        let matches = profileInfo['matches'];
+        let matchItems = [];
+        
+        let userData = await getEntry('uwuJFRo9GcO5D8YGWz6E3FjbXNj1');
+        console.log(userData);
+
+        // matches.forEach((id) => {
+        //     let userData = getEntry(id);
+        //     console.log(userData);
+        // });
+
+        // console.log(matchItems);
+
+        let matchDisplay = matchItems.map((item, index) => {
+            return(<div key={index} className="modal-profile-matches">
+                <div className="modal-matches-left">
+                    {item}
+                </div>
+                <div className="modal-matches-right">
+                    <HighlightOffIcon className="match-delete" color="secondary" fontSize="large" />
+                </div>
+                </div>); 
+        });
+
+        open();
+        setContent(matchDisplay);
+    }
+
     return (
         <div className="profile-container" data-aos="fade-up" data-aos-duration="2000">
             <div className="profile">
@@ -105,25 +137,27 @@ function Profile() {
             </Tooltip>
             <input type="file" accept="image/*" onChange={changeFile}  ref={selectFile} style={{display: 'none'}} />
             Profile Page:
-            <Typography variant="h5">Bio: <TextField label="Bio"
+            <Typography variant="h5">Bio: <TextField fullWidth label="Bio"
             name="bio" value={profileInfo.bio} disabled={!isEditing} onChange={handleChange}/></Typography>
             <br />
-            <Typography variant="h5">Name: <TextField label="Name"
+            <Typography variant="h5">Name: <TextField fullWidth label="Name"
             name="name" value={profileInfo.name} disabled={!isEditing} onChange={handleChange}/></Typography>
             <br />
-            <Typography variant="h5">Age: <TextField label="Age"
+            <Typography variant="h5">Age: <TextField fullWidth label="Age"
             name="age" value={profileInfo.age} disabled={!isEditing} onChange={handleChange}/></Typography>
             <br />
-            <Button variant="contained" color="primary" disabled={isEditing} onClick={handleEdit}>EDIT</Button> 
-            <Button variant="contained" color="secondary" disabled={!isEditing} onClick={handleSave}>SAVE</Button>
-            <br />
-            <br />
-            <Button variant="contained" color="secondary">
-                <Typography variant="h5">Matches: {profileInfo.matches.length}</Typography>
-            </Button>
+            <div className="profile-control">
+                <Button variant="contained" color="primary" disabled={isEditing} onClick={handleEdit}>EDIT</Button> 
+                <Button variant="contained" color="secondary" disabled={!isEditing} onClick={handleSave}>SAVE</Button>
+                <br />
+                <br />
+                <Button variant="contained" color="secondary" onClick={displayMatch}>
+                    <Typography variant="h5">Matches: {profileInfo.matches.length}</Typography>
+                </Button>
+            </div>
             </div>
         </div>
     )
 }
 
-export default Profile
+export default Profile;
